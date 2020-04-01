6b5f0549fe606afbc4212394e61ea8fe5991ce8d
AshkanNadiri <geekwise.ashkan.nadiri@gmail.com>
2020-04-01 10:59:10 -0700

    test

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { SignUpForm } from "./SignUpForm";
 import einstein from "./einstein.png";
 
