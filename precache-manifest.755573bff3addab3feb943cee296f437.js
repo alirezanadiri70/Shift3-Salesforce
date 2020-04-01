9230b17203ec1240f066e4f24e17d54c6e936e9c
AshkanNadiri <geekwise.ashkan.nadiri@gmail.com>
2020-04-01 11:54:05 -0700

    Updates

---
+++ precache-manifest.755573bff3addab3feb943cee296f437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593a66c7a9e18577f4bbfdbf0d55d49d",
+    "url": "/Shift3-Salesforce/index.html"
+  },
+  {
+    "revision": "b6bf87062049f520867b",
+    "url": "/Shift3-Salesforce/static/css/main.0ecf36f7.chunk.css"
+  },
+  {
+    "revision": "43306f460b71bcf99436",
+    "url": "/Shift3-Salesforce/static/js/2.6f69ee64.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shift3-Salesforce/static/js/2.6f69ee64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6bf87062049f520867b",
+    "url": "/Shift3-Salesforce/static/js/main.8b989a57.chunk.js"
+  },
+  {
+    "revision": "18b19fb7f1d6fb32b46c",
+    "url": "/Shift3-Salesforce/static/js/runtime-main.b706ea35.js"
+  },
+  {
+    "revision": "858c260dc203f678143e781667f89099",
+    "url": "/Shift3-Salesforce/static/media/einstein.858c260d.png"
+  }
+]);
